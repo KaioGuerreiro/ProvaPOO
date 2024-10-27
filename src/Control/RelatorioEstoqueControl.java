0163ab37bbab52b0fd70614e18f7f3d6dbaf66aa
Kaio Rodrigo Guerreiro Gomes <guerreirokaio@gmail.com>
2024-10-27 17:59:37 -0400

    Atualizaçao getAllProducts

--- src/Control/RelatorioEstoqueControl.java
+++ src/Control/RelatorioEstoqueControl.java
@@ -2,15 +2,31 @@ package Control;
 
 import Model.Categoria;
 import Model.ProdRelatorio;
+import Model.Produto;
 import Persistence.Dados;
 
 import java.util.ArrayList;
 
 public class RelatorioEstoqueControl {
-    static ArrayList<ProdRelatorio> getAllProducts(){
+    static ArrayList<ProdRelatorio> getAllProducts(Dados dados){
         ArrayList<ProdRelatorio> listaDeProdutos = new ArrayList<>();
-        ProdRelatorio tempProdRelatorio = new ProdRelatorio();
         ArrayList<Categoria> listaDeCategoria = new ArrayList<>();
+
+        listaDeCategoria = dados.getCategorias(); //copia todas as categorias armazenadas em dados
+
+        for (Categoria cat : listaDeCategoria){
+            String nomeCategoria;
+            nomeCategoria = cat.getNome();
+            for (Produto p : cat.getProdutos()){
+                if (p.isExcluido()){
+                    continue;
+                }
+                ProdRelatorio tempProdRelatorio = new ProdRelatorio(nomeCategoria, p.getCodigo(), p.getQuantidadeEstoque(),
+                        p.getQuantidadeMin(), p.getPreco(), p.getFornecedor().getNome(), p.getFornecedor().getContato());
+
+                listaDeProdutos.add(tempProdRelatorio);
+            }
+        }
 
         return listaDeProdutos;
     }
--- src/Model/Fornecedor.java
+++ src/Model/Fornecedor.java
@@ -4,4 +4,19 @@ public class Fornecedor {
     private String nome;
     private String contato;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
 }
